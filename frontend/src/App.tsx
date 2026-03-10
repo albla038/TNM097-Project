18a565ccaa255bc80322a42615d4ac2f8787fd73
Albert Lännerström <albert.lannerstrom@gmail.com>
2026-03-10 13:41:00 +0100

    feat: Add frontend starting point with shadcn/ui and Vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export function App() {
+  return <div className="flex min-h-svh p-6">
+    <h1>Open PBN</h1>
+  </div>;
+}
+
+export default App;
